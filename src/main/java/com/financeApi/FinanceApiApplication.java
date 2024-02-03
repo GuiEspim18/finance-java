304e14be34d178d5266b397b66b207426bdc63a0
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-02-03 10:38:12 -0300

    api request

--- src/main/java/com/financeApi/FinanceApiApplication.java
+++ src/main/java/com/financeApi/FinanceApiApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @SpringBootApplication
 public class FinanceApiApplication implements CommandLineRunner {
 
@@ -14,8 +19,18 @@ public class FinanceApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		final String KEY = Auth.getKey();
-		System.out.println(KEY);
-	}
+		String key = Auth.getKey();
+		String symbol = "AAPL";
+		String url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + key;
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+
+		try {
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			System.out.println(response.body());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+ 	}
 
 }
